7d6c2124302a139dc8d8f5927bfa908f3d2653ae
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-12-15 02:57:04 +0900

    Small fix

--- flake.nix
+++ flake.nix
@@ -8,46 +8,49 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = {
-    self,
-    nixpkgs,
-    neovim-flake,
-    flake-utils,
-    systems,
-    cc-server,
-  }: let
-    systems = import ./systems.nix;
-    buildEfi = pkgs: pkgs.callPackage ./. {};
-  in
+  outputs =
+    { self
+    , nixpkgs
+    , neovim-flake
+    , flake-utils
+    , cc-server
+    ,
+    }:
+    let
+      buildEfi = pkgs: pkgs.callPackage ./. { };
+    in
     {
-      overlays.default = final: prev: {efi-power = buildEfi prev.pkgs;};
-      devShells.x86_64-linux.default = let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        editor = neovim-flake.packages.x86_64-linux.nix.extendConfiguration {
-          modules = [
-            {
-              vim.languages.clang.enable = true;
-              vim.git.gitsigns.codeActions = false;
-            }
-          ];
-          inherit pkgs;
-        };
+      overlays.default = final: prev: { efi-power = buildEfi prev.pkgs; };
+      devShells.x86_64-linux.default =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          editor = neovim-flake.packages.x86_64-linux.nix.extendConfiguration {
+            modules = [
+              {
+                vim.languages.clang.enable = true;
+                vim.git.gitsigns.codeActions = false;
+              }
+            ];
+            inherit pkgs;
+          };
 
-        shell = (buildEfi pkgs).overrideAttrs (o: {
-          OVMF_DIR = "${pkgs.OVMF.fd}/FV";
-          nativeBuildInputs = with pkgs; [editor socat qemu];
-        });
-      in
+          shell = (buildEfi pkgs).overrideAttrs (o: {
+            OVMF_DIR = "${pkgs.OVMF.fd}/FV";
+            nativeBuildInputs = with pkgs; [ editor socat qemu ];
+          });
+        in
         shell;
     }
-    // flake-utils.lib.eachSystem (import systems) (
-      system: let
+    // flake-utils.lib.eachSystem (import ./systems.nix) (
+      system:
+      let
         supportsLegacy = builtins.elem system nixpkgs.lib.systems.flakeExposed;
         pkgs =
           if supportsLegacy
           then nixpkgs.legacyPackages.${system}
-          else import nixpkgs {inherit system;};
-      in {
+          else import nixpkgs { inherit system; };
+      in
+      {
         packages = {
           default = buildEfi pkgs;
         };
